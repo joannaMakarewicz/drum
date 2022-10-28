3b1ea990f2410459c83d1fe34e6151108aeae467
Joanna Makarewicz <joanna.milun@gmail.com>
2022-10-28 09:25:51 +0200

    spinning off a part of the JS-practice project into a separate Drum project

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,17 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!');
+
+const keys = document.querySelectorAll('button');
+let sounds = document.querySelector('.first__clap--js');
+
+for(let element of keys){
+    element.addEventListener('click', ()=>{
+        element.classList.toggle('first__special');   
+    })
+    element.addEventListener('click', (e) => {
+        sounds.play();
+    })
+}
