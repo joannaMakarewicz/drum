c107ea2449abfb10c2f75b16aa0e0011fa6837e0
Joanna Makarewicz <joanna.milun@gmail.com>
2022-10-28 14:54:49 +0200

    button press function for sound and visual effect

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,23 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 /* place your code below */
 
-console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!');
+console.log("HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!");
 
-const keys = document.querySelectorAll('button');
-let sounds = document.querySelector('.first__clap--js');
+const keys = document.querySelectorAll("button");
+const audios = document.querySelectorAll("audio");
 
-for(let element of keys){
-    element.addEventListener('click', ()=>{
-        element.classList.toggle('first__special');   
-    })
-    element.addEventListener('click', (e) => {
-        sounds.play();
-    })
+for (let element of keys) {
+  element.addEventListener("click", () => {
+    element.classList.toggle("first__special");
+    for (let audio of audios) {
+      if (audio.id == element.id) {
+        audio.currentTime = 0;
+        audio.play();
+      }
+    }
+  });
 }
+
+
+
