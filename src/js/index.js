0d9eb59bb21b4d975ae53a5184bd2ac8e51912ea
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-02 15:08:55 +0100

    loading history

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,8 @@ console.log("HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!");
 
 const buttons = document.querySelectorAll("button");
 const audios = document.querySelectorAll("audio");
-const history = document.querySelector(".music__history");
+const history = document.querySelector(".music__history--js");
+const load = document.querySelector(".history__button--js");
 
 function playSound(e) {
   audios.forEach((audio) => {
@@ -38,5 +39,12 @@ for (let button of buttons) {
     }
     const myTempate = `<button class="first__button">${button.innerHTML}</button>`;
     history.innerHTML += myTempate;
+    localStorage.setItem('sound', JSON.stringify(history.innerHTML));
+
   });
 }
+
+load.addEventListener('click', () => {
+  const myResult=localStorage.getItem('sound');
+  history.innerHTML=JSON.parse(myResult);
+})
